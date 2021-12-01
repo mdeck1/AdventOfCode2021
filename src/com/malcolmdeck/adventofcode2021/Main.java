462b7aca0570c89e7f7e3d7e1d0b278725cbfee2
mdeck1 <malcolmdeck@gmail.com>
2021-11-30 21:09:08 -0800

    Day 1 solution

--- src/com/malcolmdeck/adventofcode2021/Main.java
+++ src/com/malcolmdeck/adventofcode2021/Main.java
@@ -5,6 +5,6 @@ import com.malcolmdeck.adventofcode2021.levels.level1.Level1;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Level1.run();
+        Level1.partTwo();
     }
 }
--- src/com/malcolmdeck/adventofcode2021/levels/level1/Level1.java
+++ src/com/malcolmdeck/adventofcode2021/levels/level1/Level1.java
@@ -8,13 +8,44 @@ import java.util.Scanner;
 
 public class Level1 {
 
-    public static void run() throws Exception {
+    public static void partOne() throws Exception {
         File file = FileHelper.getFile("level1\\level1data.txt");
         try {
             Scanner scanner = new Scanner(file);
+            int prev = scanner.nextInt();
+            int count = 0;
             while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+                int curr = scanner.nextInt();
+                if (prev < curr) {
+                    count++;
+                }
+                prev = curr;
             }
+            System.out.println("Number of depth increases: " + count);
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFound: " + e.getMessage());
+        }
+        return;
+    }
+
+    public static void partTwo() throws Exception {
+        File file = FileHelper.getFile("level1\\level1data.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            int prev3 = scanner.nextInt();
+            int prev2 = scanner.nextInt();
+            int prev1 = scanner.nextInt();
+            int count = 0;
+            while (scanner.hasNextLine()) {
+                int curr = scanner.nextInt();
+                if (prev3 < curr) {
+                    count++;
+                }
+                prev3 = prev2;
+                prev2 = prev1;
+                prev1 = curr;
+            }
+            System.out.println("Number of window increases: " + count);
         } catch (FileNotFoundException e) {
             System.out.println("FileNotFound: " + e.getMessage());
         }
